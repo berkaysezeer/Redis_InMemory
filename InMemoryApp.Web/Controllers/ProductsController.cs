14080b74b6d86985768fb598cf45139c01095b74
Berkay Sezer <berkaysezeer@gmail.com>
2023-10-26 22:18:24 +0300

    InMemory Get(), Set(), TryGetValue(), Remove(), GetOrCreate()

--- InMemoryApp.Web/Controllers/ProductsController.cs
+++ InMemoryApp.Web/Controllers/ProductsController.cs
@@ -14,12 +14,31 @@ namespace InMemoryApp.Web.Controllers
 
         public IActionResult Index()
         {
-            _memoryCache.Set<string>("Zaman", DateTime.Now.ToString());
+            //Cahce kontrol etmek için 1.yol
+            if (String.IsNullOrEmpty(_memoryCache.Get<string>("Zaman")))
+                _memoryCache.Set<string>("Zaman", DateTime.Now.ToString());
+
+            //Cahce kontrol etmek için 2.yol
+            //eğer true dönerse zamanCache'e değeri atar
+            if (!_memoryCache.TryGetValue("Zaman", out string zamanCache))
+                _memoryCache.Set<string>("Zaman", DateTime.Now.ToString());
+
+
             return View();
         }
 
         public IActionResult MemoryGet()
         {
+            //cache silmek için kullanılır
+            _memoryCache.Remove("Zaman");
+
+            //Get ile değeri almaya çalışır, eğer yok ise oluşturur
+            //var mı yok mu kontrol etmeye gerek yok
+            _memoryCache.GetOrCreate<string>("Zaman", enrty =>
+            {
+                return DateTime.Now.ToString();
+            });
+
             ViewBag.Zaman = _memoryCache.Get<string>("Zaman");
             return View();
         }
