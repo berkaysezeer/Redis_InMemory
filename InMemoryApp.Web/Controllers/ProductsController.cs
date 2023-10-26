ca0f85280a485cf5c12c329b1d46ea4c82d2c25c
Berkay Sezer <berkaysezeer@gmail.com>
2023-10-26 22:43:57 +0300

    MemoryCacheEntryOptions (SlidingExpiration, AbsoluteExpiration)

--- InMemoryApp.Web/Controllers/ProductsController.cs
+++ InMemoryApp.Web/Controllers/ProductsController.cs
@@ -15,14 +15,24 @@ namespace InMemoryApp.Web.Controllers
         public IActionResult Index()
         {
             //Cahce kontrol etmek için 1.yol
-            if (String.IsNullOrEmpty(_memoryCache.Get<string>("Zaman")))
-                _memoryCache.Set<string>("Zaman", DateTime.Now.ToString());
+            //if (String.IsNullOrEmpty(_memoryCache.Get<string>("Zaman")))
+            //    _memoryCache.Set<string>("Zaman", DateTime.Now.ToString());
 
             //Cahce kontrol etmek için 2.yol
             //eğer true dönerse zamanCache'e değeri atar
-            if (!_memoryCache.TryGetValue("Zaman", out string zamanCache))
-                _memoryCache.Set<string>("Zaman", DateTime.Now.ToString());
+            //if (!_memoryCache.TryGetValue("Zaman", out string zamanCache))
+            //{
 
+            //}
+
+            //Zaman cache'inin ömrünü ayarlıyoruz
+            MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+            options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+
+            //eğer 10 saniye içinde erişilmezse silinir, erişilirse 10 saniye daha uzar
+            options.SlidingExpiration = TimeSpan.FromSeconds(10);
+
+            _memoryCache.Set<string>("Zaman", DateTime.Now.ToString(), options);
 
             return View();
         }
@@ -30,16 +40,20 @@ namespace InMemoryApp.Web.Controllers
         public IActionResult MemoryGet()
         {
             //cache silmek için kullanılır
-            _memoryCache.Remove("Zaman");
+            //_memoryCache.Remove("Zaman");
 
             //Get ile değeri almaya çalışır, eğer yok ise oluşturur
             //var mı yok mu kontrol etmeye gerek yok
-            _memoryCache.GetOrCreate<string>("Zaman", enrty =>
-            {
-                return DateTime.Now.ToString();
-            });
+            //_memoryCache.GetOrCreate<string>("Zaman", enrty =>
+            //{
+            //    return DateTime.Now.ToString();
+            //});
 
-            ViewBag.Zaman = _memoryCache.Get<string>("Zaman");
+            //ViewBag.Zaman = _memoryCache.Get<string>("Zaman");
+
+            _memoryCache.TryGetValue("Zaman", out string zamanCache);
+            ViewBag.Zaman = zamanCache;
+
             return View();
         }
     }
