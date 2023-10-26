31249cf376c1a26036bfbb567e2c50e50d529a6f
Berkay Sezer <berkaysezeer@gmail.com>
2023-10-26 22:10:02 +0300

    Add project files.

---
+++ InMemoryApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryApp.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IMemoryCache _memoryCache;
+
+        public ProductsController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            _memoryCache.Set<string>("Zaman", DateTime.Now.ToString());
+            return View();
+        }
+
+        public IActionResult MemoryGet()
+        {
+            ViewBag.Zaman = _memoryCache.Get<string>("Zaman");
+            return View();
+        }
+    }
+}
