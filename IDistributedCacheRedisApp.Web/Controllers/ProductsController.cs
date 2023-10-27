2639f7e6898528156e5935078ab9b181b48b532e
Berkay Sezer <berkaysezeer@gmail.com>
2023-10-27 23:29:31 +0300

    SetString, Remove, GetString, async

---
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace IDistributedCacheRedisApp.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public ProductsController(IDistributedCache distributedCache) => _distributedCache = distributedCache;
+
+        public async Task<IActionResult> Index()
+        {
+            DistributedCacheEntryOptions entryOptions = new DistributedCacheEntryOptions()
+            {
+                AbsoluteExpiration = DateTime.Now.AddMinutes(2)
+            };
+
+            _distributedCache.SetString("FirstStringCache", "Berkay Sezer", entryOptions);
+            await _distributedCache.SetStringAsync("AsyncCache", "Berkay", entryOptions);
+
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            ViewBag.Cache = _distributedCache.GetString("FirstStringCache");
+            return View();
+        }
+
+        public IActionResult Remove()
+        {
+            _distributedCache.Remove("FirstStringCache");
+            return View();
+        }
+    }
+}
