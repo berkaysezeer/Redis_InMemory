cda00b434ed7233555a563177f3e9cace1686200
Berkay Sezer <berkaysezeer@gmail.com>
2023-10-29 23:49:39 +0300

    Hash Type => HashGetAll(), HashSet(), HashDelete()

---
+++ StackExchangeApi.Web/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using StackExchangeApi.Web.Services;
+
+namespace StackExchangeApi.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        private readonly RedisService _redisService;
+        protected readonly IDatabase db;
+
+        public BaseController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(1);
+        }
+
+    }
+}
---
+++ StackExchangeApi.Web/Controllers/HashTypeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using StackExchangeApi.Web.Services;
+
+namespace StackExchangeApi.Web.Controllers
+{
+    public class HashTypeController : BaseController
+    {
+        private string cacheKey { get; set; } = "sozluk";
+
+        public HashTypeController(RedisService redisService) : base(redisService)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            Dictionary<string, string> list = new();
+
+            if (db.KeyExists(cacheKey))
+            {
+                db.HashGetAll(cacheKey).ToList().ForEach(x =>
+                {
+                    list.Add(x.Name, x.Value);
+                });
+            }
+
+            return View(list);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name, string value)
+        {
+            db.HashSet(cacheKey, name, value);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Remove(string key)
+        {
+            await db.HashDeleteAsync(cacheKey, key);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
