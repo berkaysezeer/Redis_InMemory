e9db41bc5ebe1cd43981b819bb7afae38b1427bd
Berkay Sezer <berkaysezeer@gmail.com>
2023-10-29 19:55:18 +0300

    StringGet(), StringIncrement(), StringSet()

---
+++ StackExchangeApi.Web/Controllers/StringTypeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using StackExchangeApi.Web.Services;
+
+namespace StackExchangeApi.Web.Controllers
+{
+    public class StringTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase db;
+
+        public StringTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(0);
+        }
+
+        public IActionResult Index()
+        {
+            //db.KeyDelete("name");
+            db.StringSet("name", "Berkay Sezer");
+            db.StringSet("ziyaretci", 100);
+
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            var name = db.StringGet("name");
+            if (name.HasValue) ViewBag.Name = name.ToString();
+
+            var ziyaretci = db.StringIncrement("ziyaretci");
+            ViewBag.Ziyaretci = ziyaretci.ToString();
+
+            return View();
+        }
+    }
+}
---
+++ StackExchangeApi.Web/Services/RedisService.cs
@@ -0,0 +1,25 @@
+﻿using StackExchange.Redis;
+
+namespace StackExchangeApi.Web.Services
+{
+    public class RedisService
+    {
+        private ConnectionMultiplexer _redis;
+        public IDatabase db { get; set; }
+
+        public RedisService(IConfiguration configuration)
+        {
+            string host = configuration["Redis:Host"];
+            string port = configuration["Redis:Port"];
+            var configString = $"{host}:{port}";
+
+            _redis = ConnectionMultiplexer.Connect(configString);
+        }
+
+
+        public IDatabase GetDb(int db)
+        {
+            return _redis.GetDatabase(db);
+        }
+    }
+}
