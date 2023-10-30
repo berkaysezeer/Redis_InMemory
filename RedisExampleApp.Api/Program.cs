077d998b660e1b2ad899123a3b9b460ce01efb7b
Berkay Sezer <berkaysezeer@gmail.com>
2023-10-30 20:42:11 +0300

    Program.cs AddScoped

--- RedisExampleApp.Api/Program.cs
+++ RedisExampleApp.Api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using RedisExampleApp.Api.Models;
+using RedisExampleApp.Api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IProductRepository, ProductRepository>(); //reques, response'a dönüþene kadar tek nesne örneði kullanýlmasý için
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseInMemoryDatabase("mydatabase");
