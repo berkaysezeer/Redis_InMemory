cc77cd1a95d658961e52f535bb6a4c099956a38b
Berkay Sezer <berkaysezeer@gmail.com>
2023-10-30 20:23:31 +0300

    In Memory Database Configuration

---
+++ RedisExampleApp.Api/Models/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RedisExampleApp.Api.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product() { Id = 1, Name = "Kalem", Price = 25M },
+                new Product() { Id = 2, Name = "0.5 Uç", Price = 10M },
+                new Product() { Id = 3, Name = "Silgi", Price = 15.5M }
+                );
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
