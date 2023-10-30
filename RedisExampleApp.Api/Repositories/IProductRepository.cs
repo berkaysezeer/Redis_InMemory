8db1577e6d38dce9083cbe1e116cb32239e75ac6
Berkay Sezer <berkaysezeer@gmail.com>
2023-10-30 20:37:47 +0300

    IProductRepository, ProductRepository, Products Controller

---
+++ RedisExampleApp.Api/Repositories/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using RedisExampleApp.Api.Models;
+
+namespace RedisExampleApp.Api.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> GetListAsync();
+        Task<Product> GetByIdAsync(int Id);
+        Task<Product> CreateAsync(Product product);
+    }
+}
---
+++ RedisExampleApp.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RedisExampleApp.Api.Models;
+
+namespace RedisExampleApp.Api.Repositories
+{
+
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProductRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> CreateAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> GetByIdAsync(int Id)
+        {
+            return await _context.Products.FindAsync(Id);
+        }
+
+        public async Task<List<Product>> GetListAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}
